e4628f006b161701b5b458d479195f9c915bed21
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2025-03-24 14:55:52 -0700

    modify model.py - modify model basefile for better consistent with OOP practice

--- python/Model.py
+++ python/Model.py
@@ -1,18 +1,20 @@
-
+import os 
 
 class  Model:
     def __init__(self):
-        pass
+        self.model = None
+        self.model_dir = "models"
 
-    def Train(self, image, label):
-        pass
+    def Train(self, df, **kwargs):
+        raise NotImplementedError("Train method not implemented. Implement in child class")
 
-    def Predict(self, image = None):
-        predictedLabel = None
-        print("Model Predicted:")
-        return predictedLabel
+    def Predict(self, df = None):
+        if df is None:
+            raise ValueError("Input data is required for prediction")
+        else:
+            return None
 
     def Preprocess(self, df):
-        pass
+        return df
 
 
